0d7f375b0d09beab07f1de9ae459e0601acce018
Zenus004 <satyajyoti05@gmail.com>
2024-07-31 10:18:45 +0530

    full project

---
+++ landing_page.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+document.querySelector('.menu').addEventListener('click', () => {
+  document.querySelector('.nav-collaps').classList.toggle('show');
+});
